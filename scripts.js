fc930cff7b596ee3ac4deec7f01801775c9e9247
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-02-03 22:42:07 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,40 +1,36 @@
 const mario = document.querySelector('.mario')
-const pipe = document.querySelector('.pipe')
-const botao = document.querySelector('.botao')
+const cano = document.querySelector('.cano')
+const cima = document.querySelector('.cima')
 const reset = document.querySelector('.reset')
 const gameover = document.querySelector('.gameover')
 
-const jump = () => {
-	mario.classList.add('jump')
+const pular = () => {
+  mario.classList.add('pulo')
 
-	setTimeout(() => {
-		mario.classList.remove('jump')
-	}, 500)
+  setTimeout(() => { mario.classList.remove('pulo') }, 500)
 }
 
-const loop = setInterval(() => {
-	const pipePosition = pipe.offsetLeft
-	const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+const colidir = setInterval(() => {
+  const posCano = cano.offsetLeft
+  const posMario = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-	if(pipePosition <= 60 && pipePosition > 0 && marioPosition < 40) {
-		pipe.style.animation = 'none'
-		pipe.style.left = `${pipePosition}px`
+  if(posCano <= 60 && posCano > 0 && posMario < 40) {
+    cano.style.animation = 'none'
+    cano.style.left = `${posCano}px`
 
-		mario.style.animation = 'none'
-		mario.style.bottom = `${marioPosition}px`
+    mario.style.animation = 'none'
+    mario.style.bottom = `${posMario}px`
 
-		mario.src = 'imagens/game-over.png'
-		mario.style.width = '38px'
-		mario.style.marginLeft = '25px'
+    mario.src = 'imagens/game-over.png'
+    mario.style.width = '38px'
+    mario.style.marginLeft = '25px'
 
     gameover.style.visibility = 'visible'
 
-		clearInterval(loop)
-	}
+    clearInterval(colidir)
+  }
 }, 10)
 
-
-
-document.addEventListener('keydown', jump)
-botao.addEventListener('click', jump)
+document.addEventListener('keydown', pular)
+cima.addEventListener('click', pular)
 reset.addEventListener('click', () => { location.reload() })
